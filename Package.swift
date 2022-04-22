3a94d710f20820211249de23a3d01b79ec4bf85b
gokulnair2001 <gokulnair.2001@gmail.com>
2022-04-22 19:26:09 +0530

    [Unit Test] -> Commented Out

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KeychainManager",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "KeychainManagerTests",
             dependencies: ["KeychainManager"]),
